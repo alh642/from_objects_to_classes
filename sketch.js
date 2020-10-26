7c6bbcd25aedd09d45e28a3a79b556031472a61c
jnyiu <71399772+alh642@users.noreply.github.com>
2020-10-26 11:50:34 -0500

    balls drift horizontally

--- sketch.js
+++ sketch.js
@@ -4,24 +4,27 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   balls.ball0 = {
     x: random(width),
-    y: random(50),
-    dy: 0,
+    y: random(height),
+    dy: (-1, 1),
   }
   balls.ball1 = {
     x: random(width),
-    y: random(50),
-    dy: 0,
+    y: random(height),
+    dy: (-1, 1),
   }
 }
 
 function draw() {
   background(200);
+  
+  balls.ball0.x += balls.ball0.dy;
   balls.ball0.y += balls.ball0.dy;
-  balls.ball0.dy += 0.25;
   circle(balls.ball0.x, balls.ball0.y, 50);
+  
+  balls.ball1.x += balls.ball1.dy;
   balls.ball1.y += balls.ball1.dy;
-  balls.ball1.dy += 0.25;
   circle(balls.ball1.x, balls.ball1.y, 50);
+  
   // if the object hits the bottom of the canvas
   // make it go up instead of down
   if (balls.ball0.y >= height) {
